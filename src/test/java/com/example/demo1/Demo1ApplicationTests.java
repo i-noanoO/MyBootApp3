f586ff71ff6eaaea7eea478b005c1cd0ecdc1224
i-noanoO <te210584@gmail.com>
2024-06-11 10:29:13 +0900

    課題5登録

--- src/test/java/com/example/demo1/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo1/Demo1ApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.demo1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Demo1ApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
